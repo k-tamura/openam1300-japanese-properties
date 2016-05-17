4fc81803499b4815d530e5502916593900fe82c0
Kohei Tamura <ktamura.biz.80@gmail.com>
2016-05-17 13:00:50 +0900

    Update build-openam-jp.sh

--- build-openam-jp.sh
+++ build-openam-jp.sh
@@ -8,14 +8,21 @@ fi
 wget --no-check-certificate https://codeload.github.com/k-tamura/openam1300-japanese-properties/zip/master -O openam1300-japanese-properties-master.zip
 
 # Copy the Japanese properties to OpenAM 13 source code.
+rm -fr openam-13.0.0
 unzip 13.0.0.zip
-unzip master.zip
+unzip openam1300-japanese-properties-master.zip
 rm openam1300-japanese-properties-master/README.*
 rm openam1300-japanese-properties-master/*.sh
 \cp -r openam1300-japanese-properties-master/* openam-13.0.0/
 rm -fr openam1300-japanese-properties-master
-
-# Build OpenAM
 cd openam-13.0.0
+
+# Read a user input (OpenAM installation directory).
+echo "Please input path to the mvn command if needed. For example, /usr/local/apache-maven-3.1.0/bin/mvn"
+read MVN_CMD
+if [ "$MVN_CMD" = "" ]; then
+    MVN_CMD=mvn
+fi
+# Build OpenAM
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
-mvn -DskipTests=true clean install
+$MVN_CMD -DskipTests=true clean install
